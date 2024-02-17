bf4654726a0c38af1b1cc165bbe1794128352b57
Sriram Swaminarayan <sriram@lanl.gov>
2024-02-16 17:31:26 -0700

    First cut at addtoindex.  Not ocrrect though.

--- src/maltReaderJSON.cpp
+++ src/maltReaderJSON.cpp
@@ -1,6 +1,8 @@
+#include <cstdint>
+#include <sstream>
 
+#include "json/json.h"
 #include "maltReaderJSON.hpp"
-#include <json/json.h>
 
 MaltReaderJSON::MaltReaderJSON(const char *fname) : fname_(fname) {
   std::ifstream ifs;
@@ -35,6 +37,75 @@ MaltReaderJSON::MaltReaderJSON(const char *fname) : fname_(fname) {
     int lineNo = (iDict.isMember("line") ? iDict["line"].asInt() : -1);
     instrMap_[item] = {.function = idFunction, .file = idFile, .line = lineNo};
     nameMap_[idFunction].push_back(item);
+  }
+
+  index_();
+}
+
+void MaltReaderJSON::addToIndex_(std::string name, int count, size_t inclusive,
+				 size_t exclusive, size_t globalPeak){
+  //< Utility routint that enables indexed searching for higher speed
+  if (count_.find( name ) == count_.end()) {
+    count_[name] = 0;
+    inclusive_[name] = exclusive_[name] = 0;
+  }
+  count_[name] += count;
+  inclusive_[name] += inclusive;
+  exclusive_[name] += exclusive;
+
+  if (globalPeak > 0) { 
+    if (globalPeak_.find( name ) == globalPeak_.end()) {
+      globalPeak_[name] = std::array<size_t,2>{0,0};
+    }
+    globalPeak_[name][0] += globalPeak;
+    if (exclusive > 0) {
+      globalPeak_[name][1] += globalPeak;
+    }
+  }
+}
+
+void MaltReaderJSON::index_() {
+  // Enables indexing for searches by creating a reverse lookup.
+  // This populates self.inclusive, self.exclusive and
+  // self.globalPeaks all of which are dictionaries with the name
+  // of a subroutine as key and the appropriate memory as value,
+  // except for self.globalPeak which is a list with
+  // [inclusiveGlobalPeak, exclusiveGlobalPeak] as values
+  std::vector<std::string> ignores({"calloc", "malloc", "posix_memalign",
+                    "realloc", "operator new(unsigned long)" });
+
+  auto &stats = data_["stacks"]["stats"];
+  for (auto &item : stats.getMemberNames()) {
+    auto &infos = stats[item]["infos"];
+    int count = infos["alloc"]["count"].asInt();
+    size_t glop, inclusive, exclusive;
+    std::stringstream(infos["alloc"]["sum"].asString()) >> inclusive;
+    std::stringstream(infos["globalPeak"].asString()) >> glop;
+    exclusive = inclusive;
+    if (inclusive == 0  and glop == 0) {
+      continue;
+    }
+    auto &theStack = stats[item]["stack"];
+    auto &theStackId = stats[item]["stackId"];
+    for (auto const &entry : theStack.getMemberNames()) {
+      bool a = false;
+      for (auto & ignore : ignores) {
+	if (ignore.compare(entry) == 0) {
+	  a = true;
+	  break;
+	}
+      }
+      if (a) { continue;}
+
+      callsite_[theStackId.asString()].push_back(entry);
+      auto &name = instrMap_[entry].function;
+      addToIndex_(name, count, inclusive, exclusive, glop);
+      exclusive = 0;
+    }
+    if (exclusive > 0) {
+      // looks like an empty stack
+      addToIndex_(theStackId.asString(), count, inclusive, exclusive, glop);
+    }
   }
 }
 
--- src/maltReaderJSON.hpp
+++ src/maltReaderJSON.hpp
@@ -1,3 +1,5 @@
+#include <array>
+#include <cstdint>
 #include <fstream>
 #include <iostream>
 #include <map>
@@ -41,4 +43,14 @@ private:
       instrMap_; //< Lookup for converting instruction to file / function / line
   std::map<std::string, std::vector<std::string>>
       nameMap_; //< Reverse lookup from function to stacks
+  std::map<std::string, int> count_;
+  std::map<std::string, size_t> inclusive_;
+  std::map<std::string, size_t> exclusive_;
+  std::map<std::string, std::array<size_t,2>> globalPeak_;
+  std::map<std::string, std::vector<std::string>> callsite_;
+
+  //Methods
+  void index_(); //< Indexes functions for easy reverse lookup
+  void addToIndex_(std::string name, int count, size_t inclusive,
+				   size_t exclusive, size_t globalPeak);
 };
