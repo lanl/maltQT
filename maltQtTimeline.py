520123c0be986d719169be71752752b0a230b1c5
Sriram Swaminarayan <sriram@lanl.gov>
2024-03-04 13:48:46 -0700

    Fixed bug in file timer that fired wrong routine

--- maltQtTimeline.py
+++ maltQtTimeline.py
@@ -306,7 +306,7 @@ class MaltQtTimeline(QWidget):
 
         self.fTimer = fTimer = QtCore.QTimer()
         fTimer.setSingleShot(True)
-        fTimer.timeout.connect(self.fileLoad)
+        fTimer.timeout.connect(self.fileShow)
 
         self.stack_view.setFocusPolicy(QtCore.Qt.NoFocus)
         self.info.setFocusPolicy(QtCore.Qt.NoFocus)
