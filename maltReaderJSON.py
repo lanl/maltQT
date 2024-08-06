ac88fe0510bffcd3faa0bee2f24cb423e23b6aad
Sriram Swaminarayan <sriram@lanl.gov>
2024-08-06 09:34:25 -0600

    Added Copyright.txt and appended copyright to each file

--- maltReaderJSON.py
+++ maltReaderJSON.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
+# LANL Open Source Release ID O4736
+#
+# Copyright:
+# © 2024. Triad National Security, LLC. All rights reserved.  This
+# program was produced under U.S. Government contract 89233218CNA000001
+# for Los Alamos National Laboratory (LANL), which is operated by Triad
+# National Security, LLC for the U.S. Department of Energy/National
+# Nuclear Security Administration. All rights in the program are
+# reserved by Triad National Security, LLC, and the U.S. Department of
+# Energy/National Nuclear Security Administration. The Government is
+# granted for itself and others acting on its behalf a nonexclusive,
+# paid-up, irrevocable worldwide license in this material to reproduce,
+# prepare. derivative works, distribute copies to the public, perform
+# publicly and display publicly, and to permit others to do so.
+
 """
 Reads in a MALT JSON file and provides a human-traversable
   MaltReaderJSON(fname, filterBy=None):
